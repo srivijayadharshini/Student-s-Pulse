fdcc53e67830760c60a72040115e295bd26754c7
dharshhpriya19 <dharshinirajesh457@gmail.com>
2025-10-06 21:45:33 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",  
-		"./app/**/*.{ts,tsx}", 
+		"./app/**/*.{ts,tsx}",  
 		"./src/**/*.{ts,tsx}",
 	], 
 	prefix: "",
